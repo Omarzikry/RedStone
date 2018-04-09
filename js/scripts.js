9e0ba027c74d7cc9dc57b4572aa982f390cd35e4
Omar <omar_zikry@github.com>
2018-04-10 00:45:25 +0200

    updates

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// === nav btn === //
+
+$('.nav-btn').click(function(){
+    $('.nav-btn').toggleClass('open');
+    $('.sidebar').toggleClass('showing');
+    $('.logo').toggleClass('open-logo')
+})
