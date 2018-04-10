26fe373f531fb836442408700a1ad4f55de5c59b
Omar <omar_zikry@github.com>
2018-04-11 00:14:10 +0200

    I just finished the home page feeling really proud

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,38 @@ $('.nav-btn').click(function(){
     $('.sidebar').toggleClass('showing');
     $('.logo').toggleClass('open-logo')
 })
+
+// === slider === //
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 7,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        focusOnSelect: true,
+        pauseOnFocus: false,
+        responsive: [
+          {
+            breakpoint: 1200,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '0px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 500,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '0px',
+              slidesToShow: 1,
+            }
+          }
+        ]
+    });
+  });
